c55f1efbd22b571f8316c8dcd959eed9e464a1c5
tojathedog <40531277+tojathedog@users.noreply.github.com>
2021-03-13 17:56:01 -0700

    rto-002: add the actual ssh-keygen part, it's silent

--- rotato.py
+++ rotato.py
@@ -3,3 +3,16 @@
 # rotato -  rotate my ssh keys because i am a lazy dog
 #
 # tojathedog 🐶
+
+import sys
+import os
+import subprocess
+import time
+
+poptime=time.strftime("%m-%d-%y-%I-%M")
+
+subprocess.run(["ssh-keygen", "-q", "-N''", "-b 4096", "-ftest-" + poptime])
+
+os.chmod("test-" + poptime, 0o600)
+os.chmod("test-" + poptime + ".pub", 0o600)
+
